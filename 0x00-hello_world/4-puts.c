1eefe9776cbf94d1af7da1ccc7624345fafdae0f
Ruthdavid <ruthyndukudavid@gmail.com>
2023-03-16 23:46:52 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
  * main - a program that prints a line with a puts function
- * Return: 0 (Succes)
+ * Return: 0 (Success)
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
-root@c817205e4147:/alx-low_level_programming/0x00-hello_world# 
