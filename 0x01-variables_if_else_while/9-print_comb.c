3278d28cd225d398f8ee2a76fbe47fbf3ca6365c
Ruthdavid <ruthyndukudavid@gmail.com>
2023-03-20 01:05:59 -0700

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,11 +10,11 @@ int main(void)
 
 	for (a = 0; a < 10; a++)
 	{
-		putchar(a + '0');
+		putchar(a);
 		if (a < 9)
 		{
 			putchar(',');
-			putchar('');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
