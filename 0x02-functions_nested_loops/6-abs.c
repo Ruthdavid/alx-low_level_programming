abf7b5194bf9b7e585b7e486d866107fa3e04f08
Ruthdavid <ruthyndukudavid@gmail.com>
2023-03-21 10:22:47 -0700

    integers

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - checks absolute value of integer
+ * @i: parameter to be checked
+ *
+ * Return: Always 0
+ */
+
+int _abs(int) 
+{
+	if (a < 0)
+		a = -(a);
+	else if (a >= 0)
+		a = a;
+	return (a);
+}
+
