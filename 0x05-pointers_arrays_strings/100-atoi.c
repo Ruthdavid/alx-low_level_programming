f288707343038b320f094a768902cbd3e46e9936
Ruthdavid <ruthyndukudavid@gmail.com>
2023-03-27 23:38:31 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * -atoi -converts a string into an integer
+ *  @s: string to be converted
+ *  Return: int converted from the string
+ */
+
+int _atoi(char *s)
+{
+	int i, d, n, len, f, digit;
+
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+
