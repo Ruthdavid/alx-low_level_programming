632f0f7192571c74fb06c4c6dcf8010112a85246
Ruthdavid <ruthyndukudavid@gmail.com>
2023-04-09 02:27:06 -0700

    token

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
  * main -Entry point
  * Return 1 (Success)
  */
-int main(void)
+int main(Void)
 {
 	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59 \r", stdout);
 	return (1);
